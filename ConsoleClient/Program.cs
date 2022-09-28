6bd9ebd53c5cc472b64fcfc94b70078244ecee96
Nazar <fedunnazar20122001@gmail.com>
2022-09-28 12:20:32 +0300

    Add project files.

---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+string ipAddressServer = "127.0.0.1";
+int portServer = 1076;
+
+IPAddress ipAddress = IPAddress.Parse(ipAddressServer);
+IPEndPoint ipEndPoint = new IPEndPoint(ipAddress, portServer);
+
+Socket client = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+client.Connect(ipEndPoint); // підключаємо(конектимо) клієнта до сервака
+
+Console.WriteLine("Client connect to server {0}", ipEndPoint);
+Console.WriteLine("Input message text");
+string message = Console.ReadLine();
+
+byte[] bytes = Encoding.UTF8.GetBytes(message);
+client.Send(bytes);
+byte[] serverResponse = new byte[1024]; // байти які пришле сервак назад клієнту
+client.Receive(serverResponse);
+string serverString = Encoding.UTF8.GetString(serverResponse);
+Console.WriteLine("Server responce \"{0}\"", serverString);
+client.Shutdown(SocketShutdown.Both);
+client.Close();
---
+++ ConsoleServer/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Net;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Text;
+
+namespace ConsoleServer
+{
+    class Program 
+    { 
+        public static void Main(string[] args)
+        {
+            string ipAddress = "127.0.0.1"; // local host ipAddress
+            int port = 1076; // 1433 не проканає, бо він зарезервований MSSQL SERVER
+            IPAddress ip = IPAddress.Parse(ipAddress); // перевіряє чи вірно введена айпішка
+            IPEndPoint endPoint = new IPEndPoint(ip, port); // кінцева точка на яку будем стукать (точка сервака ну яку ми стукаєм. ТОЧКА НА ЯКІЙ ЗАПУСТИТЬСЯ СЕРВАК)
+
+            Socket server = new Socket(ip.AddressFamily, // IPv4
+                SocketType.Stream, // буде работать в потоці і двусторонньому потоці (тобто буде отримувати запити і відповідати на них)
+                ProtocolType.Tcp); // tcp протокол - забезпечує подачу даних і перевіряє, що пакети точно доставленні (якщо не доставлені то повторно відправляє запрос). Utp - тіпа запроси в скайп (звук повідомлення ітд)
+
+            // прив'язка сервера до кінцевої точки
+            server.Bind(endPoint);
+            server.Listen(1000); // максимальна черга сервера. ТІПА скільки людей одночасно може стукатись до сервака
+
+            Console.WriteLine("Server start {0}", endPoint);
+
+            while (true) // СЕРВЕР І КЛІЄНТ ОБМІНЮЮТЬСЯ БАЙТАМИ
+            {
+                Socket client = server.Accept(); // server.Accept() -> означає, що сервер очікує запит від клієнта
+                byte[] buffer = new byte[1024]; // читать байти від клієнта буде
+                int size = client.Receive(buffer)/*вертає кількість байтів які клієнт прислав*/; // читаємо масив байтів від клієнта
+                string text = Encoding.UTF8.GetString(buffer); // перетворив байти в текст
+                Console.WriteLine($"Client: {client.RemoteEndPoint} \n Message: {text}"); // хто дав запрос і який текст 
+                byte[] clientSendData = Encoding.UTF8.GetBytes($"Лови рашист гранату {DateTime.Now}");
+                client.Send(clientSendData);
+                client.Shutdown(SocketShutdown.Both);
+                client.Close();
+            }
+        }
+    }
+}
+
